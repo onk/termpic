988cab956718ff1ed6ff093ab914c28f4e5581f7
onk <takafumi.onaka@gmail.com>
2013-01-12 16:40:23 +0900

    [test] add sample jpeg

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'termpic'
+
+module TermpicTestImage
+  sample_dir = File.expand_path(File.dirname(__FILE__) + "/sample")
+  SAMPLE_IMAGE_PATH = sample_dir + "/octocat.jpg"
+end
